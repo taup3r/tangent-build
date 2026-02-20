4ffd57c5203b11145bdd67d682f3e55725c56301
taup3r <79400123+taup3r@users.noreply.github.com>
2026-02-20 11:38:30 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,29 +10,30 @@ import { initializePortraits } from "./state.js";
 import { updateUI } from "./ui.js";
 import { playerAttack, playerDefend, playerSkill, startPlayerTurn } from "./combat.js";
 import { handleHitPress } from "./skillTiming.js";
-import { startNewBattle } from "./modal.js";
+import { openEnemyInfo } from "./modal.js";
 
 /* -------------------------
    INITIAL SETUP
 ------------------------- */
 
-window.startNewBattle = startNewBattle;
-
 window.addEventListener("DOMContentLoaded", () => {
-  
+
   // Load portraits + enemy name/hint
   initializePortraits();
 
   // Initial UI render
   updateUI();
 
-  // Hook up action buttons
+  // Player action buttons
   document.getElementById("attackBtn").addEventListener("click", playerAttack);
   document.getElementById("defendBtn").addEventListener("click", playerDefend);
   document.getElementById("skillBtn").addEventListener("click", playerSkill);
 
-  // Hit timing button
+  // Skill timing button
   document.getElementById("hitBtn").addEventListener("click", handleHitPress);
+
+  // Enemy info button
+  document.getElementById("enemyInfoBtn").addEventListener("click", openEnemyInfo);
 
   // Start the first turn
   startPlayerTurn();
