465502b3114c4bb80512a336700fa3c47607f9ae
taup3r <79400123+taup3r@users.noreply.github.com>
2026-02-23 03:31:18 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ import { openEnemyInfo, openPlayerInfoModal } from "./modal.js";
 window.addEventListener("DOMContentLoaded", () => {
 
   initializePortraits();
+  document.getElementById("playerName").textContent = player.name;
   applyStatsToCombat(player, playerStats);
   applyStatsToCombat(enemy, enemyStats);
   applyConstitution(player);
