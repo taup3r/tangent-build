7f91d4ff43c9c458f0de2d8ea6421db37395f6c3
taup3r <79400123+taup3r@users.noreply.github.com>
2026-02-26 15:48:33 +0000

    Update town.js

--- js/town.js
+++ js/town.js
@@ -1,5 +1,18 @@
 // Phase 1: Simple navigation + dungeon start
 
+export setDungeonMode(enable) {
+  if (enable) {
+    localStorage.setItem("dungeonMode", "true");
+  }
+  else {
+    localStorage.removeItem("dungeonMode");
+  }
+}
+
+export setEnemiesLeft(count) {
+  localStorage.setItem("dungeonEnemiesLeft", count);
+}
+
 document.getElementById("battleBtn").onclick = () => {
   // Normal single battle
   window.location.href = "combat.html";
@@ -7,8 +20,8 @@ document.getElementById("battleBtn").onclick = () => {
 
 document.getElementById("exploreBtn").onclick = () => {
   // Start dungeon mode with 8 enemies
-  localStorage.setItem("dungeonMode", "true");
-  localStorage.setItem("dungeonEnemiesLeft", "8");
+  setDungeonMode(true);
+  setEnemiesLeft(8);
 
   window.location.href = "combat.html";
 };
