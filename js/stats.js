7eba361982b85a198e8271c13ec3739da74747b1
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-04 22:51:31 +0000

    Create stats.js

---
+++ js/stats.js
@@ -0,0 +1,66 @@
+import { playerStats, saveProgress, loadProgress } from "./state.js";
+
+loadProgress();
+
+let tempStats = {
+  STR: playerStats.STR,
+  DEX: playerStats.DEX,
+  AGI: playerStats.AGI,
+  CON: playerStats.CON
+};
+
+let tempPoints = playerStats.statPoints;
+
+function updateUI() {
+  document.getElementById("statPointsDisplay").textContent = tempPoints;
+
+  document.getElementById("statValueSTR").textContent = tempStats.STR;
+  document.getElementById("statValueDEX").textContent = tempStats.DEX;
+  document.getElementById("statValueAGI").textContent = tempStats.AGI;
+  document.getElementById("statValueCON").textContent = tempStats.CON;
+}
+
+updateUI();
+
+document.querySelectorAll(".stat-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const stat = btn.dataset.stat;
+
+    if (btn.classList.contains("plus")) {
+      if (tempPoints > 0) {
+        tempStats[stat]++;
+        tempPoints--;
+      }
+    } else {
+      if (tempStats[stat] > playerStats[stat]) {
+        tempStats[stat]--;
+        tempPoints++;
+      }
+    }
+
+    updateUI();
+  });
+});
+
+document.getElementById("resetStatsBtn").onclick = () => {
+  tempStats = {
+    STR: playerStats.STR,
+    DEX: playerStats.DEX,
+    AGI: playerStats.AGI,
+    CON: playerStats.CON
+  };
+  tempPoints = playerStats.statPoints;
+  updateUI();
+};
+
+document.getElementById("saveStatsBtn").onclick = () => {
+  playerStats.STR = tempStats.STR;
+  playerStats.DEX = tempStats.DEX;
+  playerStats.AGI = tempStats.AGI;
+  playerStats.CON = tempStats.CON;
+  playerStats.statPoints = tempPoints;
+
+  saveProgress();
+
+  parent.postMessage("close-stats", "*");
+};
