44d2693e65d9c3590922b448e3050432f2d232e1
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-08 22:40:43 +0000

    Update town.js

--- js/town.js
+++ js/town.js
@@ -148,7 +148,8 @@ function tryQuestEncounter() {
   const blacksmith = getQuest("blacksmith");
 
   // Only trigger if quest not started
-  if (blacksmith.stage === 0 && Math.random() < (blacksmith.chance/100)) {
+  if (blacksmith.stage === 0 && Math.random() < (blacksmith.chance/100) &&
+blacksmith.stage < blacksmith.maxStage) {
     triggerQuest(blacksmith);
   }
 }
