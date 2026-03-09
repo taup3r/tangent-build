14417f46317c914b7b30832125b4445d87238afa
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-09 20:56:03 +0000

    Update town.js

--- js/town.js
+++ js/town.js
@@ -1,6 +1,7 @@
 import { player, playerStats, setDungeonMode, startDungeon, loadProgress } from "./state.js";
 import { getRandomDungeonType } from "./dungeon.js";
 import { updateHeaderStats } from "./ui.js";
+import { tryQuestEncounter } from "./quest.js";
 
 // Phase 1: Simple navigation + dungeon start
 
@@ -100,6 +101,7 @@ function generateTownLayout() {
 
 // Initial generation
 generateTownLayout();
+tryQuestEncounter();
 
 // Explore → travel
 exploreBtn.onclick = () => {
@@ -110,6 +112,7 @@ exploreBtn.onclick = () => {
   // After animation ends, regenerate and animate in
   setTimeout(() => {
     generateTownLayout();
+    tryQuestEncounter();
     randomArea.classList.remove("travel-out");
     randomArea.classList.add("travel-in");
   }, 300);
