822a8ec205fbbc310af5e850cd8f72b55fe0bca7
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-10 22:11:53 +0000

    Update quest.js

--- js/quest.js
+++ js/quest.js
@@ -35,7 +35,7 @@ export const quests = [
     id: "blacksmith",
     chance: 50,
     stage: 0, // 0 = not started
-    maxStage: 0, // 0 - disable
+    maxStage: 4, // 0 - disable
     active: false,
     data: {} // for storing hammerFound, etc.
   }
