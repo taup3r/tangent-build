c6d722c4dd9791f560cd45d1998acaa8e2972ad1
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-16 22:17:26 +0000

    Create blacksmith.js

---
+++ js/blacksmith.js
@@ -0,0 +1,13 @@
+import { player, playerStats, loadProgress, saveProgress } from "./state.js";
+import { generateWeapon } from "./weapon.js";
+import { openCompareWeapon } from "./modal.js";
+
+loadProgress();
+
+document.getElementById("refineButton").onclick = () => {
+  alert("refine here");
+};
+
+document.getElementById("backButton").onclick = () => {
+  window.location.href = `town.html?player=${encodeURIComponent(player.name)}`;
+};
