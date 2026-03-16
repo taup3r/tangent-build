f66c72696733cfcbeef2e89c095dd9c24a06a591
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-16 22:50:36 +0000

    Update blacksmith.js

--- js/blacksmith.js
+++ js/blacksmith.js
@@ -1,11 +1,21 @@
 import { player, playerStats, loadProgress, saveProgress } from "./state.js";
-//import { generateWeapon } from "./weapon.js";
+import { generateWeapon, upgradeWeapon } from "./weapon.js";
 //import { openCompareWeapon } from "./modal.js";
 
 loadProgress();
 
 document.getElementById("refineButton").onclick = () => {
-  alert("refine here");
+  const weapon = player.weapon;
+  alert(JSON.stringify(weapon));
+  let newWeapon;
+
+  if (weapon) {
+    newWeapon = upgradeWeapon(weapon, 1);
+  } else {
+    newWeapon = generateWeapon(1);
+  }
+
+  alert(JSON.stringify(newWeapon));
 };
 
 document.getElementById("backButton").onclick = () => {
