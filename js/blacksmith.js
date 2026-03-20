1762b738f57226718156a1e4d6c4f95431c27ad0
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-20 20:49:11 +0000

    Update blacksmith.js

--- js/blacksmith.js
+++ js/blacksmith.js
@@ -2,7 +2,7 @@ import { player, playerStats, loadProgress, saveProgress } from "./state.js";
 import { updateHeaderStats } from "./ui.js";
 import { generateWeapon, upgradeWeapon } from "./weapon.js";
 import { openCompareWeapon } from "./modal.js";
-import { getItem } from "./items.js";
+import { getItem, saveItems } from "./items.js";
 
 loadProgress();
 updateHeaderStats();
@@ -20,6 +20,7 @@ refineButton.onclick = () => {
   if (playerStats.gold >= 1000 && ore.count >= 1) {
     // immediately reduces ore on attempt
     ore.count -= 1;
+    saveItems();
 
     if (weapon) {
       refined = upgradeWeapon(weapon, 1);
