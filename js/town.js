572cf02ffc5fb860daada5652e01bccfc65f3276
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-21 22:29:23 +0000

    Update town.js

--- js/town.js
+++ js/town.js
@@ -123,6 +123,10 @@ function questEncounters() {
     const weapon = upgradeWeapon(player.weapon, 1);
     openCompareWeapon(weapon, "Equip", () => player.weapon = weapon);
   });
+  tryQuestEncounter("merchantGuild", 1, () => {
+    playerStats.gold += 20;
+    playerStats.reputation = (playerStats.reputation || 0) + 1;
+  });
 }
 
 function explore() {
