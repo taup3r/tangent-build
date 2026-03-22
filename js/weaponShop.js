79aedbe95c56b1933a6b338e634edb083ee0ee00
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-22 17:01:53 +0000

    Update weaponShop.js

--- js/weaponShop.js
+++ js/weaponShop.js
@@ -66,13 +66,13 @@ function renderShop() {
   const inventory = loadShopInventory();
   shopList.innerHTML = "";
 
-  list discountPercent = 0;
+  let discountPercent = 0;
   const merchantGuild = getQuest("merchantGuild");
   if (merchantGuild) {
-    //if (merchantGuild.stage >= questData["merchantGuild"].maxStage) {
+    if (merchantGuild.stage >= questData["merchantGuild"].maxStage) {
       discountPercent = 5;
       document.getElementById("discountDisplay").textContent = `You enjoy a ${discountPercent}% discount from the Merchant Guild.`;
-    //}
+    }
   }
 
   inventory.forEach((w, index) => {
