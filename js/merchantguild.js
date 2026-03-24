9da5b7c56edc96a9892981eab3501dbe67a4942c
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-24 17:59:19 +0000

    Update merchantguild.js

--- js/merchantguild.js
+++ js/merchantguild.js
@@ -2,6 +2,7 @@ import { player, playerStats, loadProgress } from "./state.js";
 import { updateHeaderStats } from "./ui.js";
 import { showQuestList } from "./quest.js";
 import { showItemList } from "./items.js";
+import { weaponShopDiscount } from "./reputation.js";
 
 const questButton = document.getElementById("questButton");
 const itemButton = document.getElementById("itemButton");
@@ -12,7 +13,7 @@ updateHeaderStats();
 
 document.getElementById("loreText").textContent = "The Merchant Guild takes regulation on all goods prices and quests undertakings";
 
-reputation.textContent = `Your current reputation is ${playerStats.reputation || 0} points`;
+reputation.textContent = `Your current reputation is ${playerStats.reputation || 0} points\nYou currently have ${weaponShopDiscount()}% discount on shops.`;
 
 questButton.onclick = () => showQuestList();
 itemButton.onclick = () => showItemList();
