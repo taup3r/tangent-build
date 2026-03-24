a68a619f2d80b7cb2bda4b1eb54780b4339e258d
taup3r <79400123+taup3r@users.noreply.github.com>
2026-03-24 22:46:59 +0000

    Update reputation.js

--- js/reputation.js
+++ js/reputation.js
@@ -5,3 +5,28 @@ export function weaponShopDiscount() {
     return 5;
   }
 }
+
+export function gainReputation(tier) {
+  let rep = playerStats.reputation || 0;
+
+  if (tier === "normal") rep += 1;
+  if (tier === "elite") rep += 2;
+  if (tier === "veteran") rep += 3;
+  if (tier === "boss") rep += 4;
+
+  if (rep > 100) rep = 100;
+
+  playerStats.reputation = rep;
+}
+
+export function loseReputation(tier) {
+  let rep = playerStats.reputation || 0;
+
+  if (tier === "normal") rep -= 3;
+  if (tier === "elite") rep -= 2;
+  if (tier === "veteran") rep -= 1;
+
+  if (rep < 1) rep = 1;
+
+  playerStats.reputation = rep;
+}
