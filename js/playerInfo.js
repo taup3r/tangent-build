5b4ee8be21bb0feec408bdddd79c7750ed2c290d
taup3r <79400123+taup3r@users.noreply.github.com>
2026-04-05 14:35:23 +0100

    Update playerInfo.js

--- js/playerInfo.js
+++ js/playerInfo.js
@@ -1,6 +1,8 @@
 import { player, playerStats } from "./state.js";
+import { loadProgress } from "./state.js";
 
 export function openPlayerInfoModal() {
+  loadProgress();
   const modal = document.getElementById("playerModal");
   modal.style.display = "flex";
   document.getElementById("playerInfoCloseBtn").onclick = () => modal.style.display = "none";
