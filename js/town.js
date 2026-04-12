8996ab102a37bc8a56c795bba4dc3f37bf22e7de
taup3r <79400123+taup3r@users.noreply.github.com>
2026-04-12 19:36:43 +0100

    Update town.js

--- js/town.js
+++ js/town.js
@@ -52,15 +52,15 @@ function getAlleyZone() {
       class: "btn-arena",
       action: () => {
         tryQuestEncounter("smuggler", 3, () => {
-          if (Math.random() < 0.05) {
-            location.reload();
-          } else {
+          //if (Math.random() < 0.05) {
+            //location.reload();
+          //} else {
             getMessage("e4", () => {
               playerStats.combatEncounter = true;
               saveProgress();
               window.location.href = `combat.html?player=${encodeURIComponent(player.name)}`;
             });
-          }
+          //}
         }, () => getMessage("h7"));
       },
       disabled: false
