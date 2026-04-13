c165e7d6e86762658dd35de780b92cfbd712586f
taup3r <79400123+taup3r@users.noreply.github.com>
2026-04-13 22:41:42 +0100

    Update town.js

--- js/town.js
+++ js/town.js
@@ -310,7 +310,7 @@ function getTownSquareZone() {
     merchantGuildDone = true;
   }
 
-  if (blacksmithDone === true && merchantGuildDone === true) {
+  //if (blacksmithDone === true && merchantGuildDone === true) {
     buttons.push({
       label: "Go to the Village",
       class: "btn-zone",
@@ -321,7 +321,7 @@ function getTownSquareZone() {
       },
       disabled: false
     });
-  }
+  //}
 
   zoneName.textContent = "Wayfarer's Rest";
   return buttons;
