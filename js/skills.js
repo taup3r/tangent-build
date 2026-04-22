7fd130387a2cfc2f0c003ab2964737806ca29fc3
taup3r <79400123+taup3r@users.noreply.github.com>
2026-04-22 20:06:01 +0100

    Create skills.js

---
+++ js/skills.js
@@ -0,0 +1,20 @@
+import { player, playerStats, loadProgress } from "./state.js";
+
+loadProgress();
+
+const playerSkills = `${player.name}_skills`;
+
+export const skillData = {
+  "bthrust": {
+    title: "Balanced Thrust",
+    description: "Deal 150% base damage with a 20% chance to gain 1 ap back.",
+    maxLevel: 1
+  }
+}
+
+export skills = [
+  {
+    id: "bthrust",
+    level: 0
+  }
+]
