aa6b3c525b9e387d898c6c05caaf0e80cff32d95
taup3r <79400123+taup3r@users.noreply.github.com>
2026-07-22 21:03:29 +0100

    Update quest.js

--- js/quest.js
+++ js/quest.js
@@ -6,13 +6,13 @@ const playerQuests = `${player.name}_quests`;
 
 export const questData = {
   "intro": {
-    title: "Welcome",
+    title: "Welcome to Wayfarer",
     type: "townSquare",
     maxStage: 1, // set to 0 to turn off quest
     flow: [
       {
         npc: "",
-        message: "You arrive in Wayfarer, a quiet town that feels safer than most — at least on the surface. But beneath its calm streets, something stirs. Smugglers move in the alleys. A shard of obsidian pulses with dark energy. And an unseen force watches from the wilds beyond. You didn’t come here seeking trouble. But trouble has already found you. Your journey begins.        You can visit the arena or explore the town and get to know rhe townsfolk to learn more.",
+        message: "You arrive in Wayfarer, a quiet town that feels safer than most — at least on the surface. But beneath its calm streets, something stirs. Smugglers move in the alleys. A shard of obsidian pulses with dark energy. And an unseen force watches from the wilds beyond. You didn’t come here seeking trouble. But trouble has already found you. Your journey begins. You can visit the arena or explore the town and get to know rhe townsfolk to learn more.",
         submit: "Go",
         nextChance: 100
       }
